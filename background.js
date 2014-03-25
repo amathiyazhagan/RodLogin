c1c669ee0e9869ed47a393453f61cd624a4aa0a8
amathiyazhagan <amathiyazhagan@servicesource.com>
2014-03-25 16:27:39 -0700

    stuck at window.location

--- background.js
+++ background.js
@@ -1,12 +1,37 @@
-function sendMessage(data){
+function click(e) {
+  alert("this alert certainly shows");  
+  console.log("But this does not");
+}
+
+ function sendMessage(data){
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	  chrome.tabs.sendMessage(tabs[0].id, data, function(response) {
-	  	console.log(response.message);
+	    console.log(response.farewell);
 	  });
 	});
 }
 
 
- 		
+	var googleAuth = new OAuth2('google', {
+  client_id: '640767095498-7684ac26cdr98tkkp6ssuc02lhtg8bj5.apps.googleusercontent.com',
+  client_secret: '5IJv9U4kQB0DQoMtjBh7Vi6b',
+  api_scope: 'https://spreadsheets.google.com/feeds'
+});
 
-		
+function callback(resp, xhr) {
+  console.log("resp:"+resp);
+};
+
+function onAuthorized() {
+  var url = 'https://spreadsheets.google.com/feeds/list/0AsecKBAmOV9CdG1XLXc4dEUzSGZwQVFaLU5vYVZkakE/od6/private/values';
+  var request = {
+    'method': 'GET',
+    'parameters': {'alt': 'json'}
+  };
+
+  // Send: GET https://docs.google.com/feeds/default/private/full?alt=json
+  //googleAuth.sendSignedRequest(url, callback, request);
+  console.log('onAuthorized');
+};
+
+googleAuth.authorize(onAuthorized);
--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
-//onclick function for login
 function callBackgroundPage(){
 	var data = {};
 	data.userid = document.getElementById('user').value+'@'+
 					document.getElementById('tenant').value+'.'+
 					document.getElementById('com').value;
 	data.password = document.getElementById('password').value;
+	console.log(data.userid);
+	console.log(data.password);
 	chrome.extension.getBackgroundPage().sendMessage(data);
 }
 document.getElementById('submit').onclick=callBackgroundPage;
