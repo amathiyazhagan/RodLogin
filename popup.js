d27d1c521720a816c4059ebc3ad233907e59f6c1
amathiyazhagan <amathiyazhagan@servicesource.com>
2014-03-26 00:47:58 -0700

    remember + more ops

--- popup.js
+++ popup.js
@@ -5,6 +5,17 @@ function callBackgroundPage(){
 					document.getElementById('tenant').value+'.'+
 					document.getElementById('com').value;
 	data.password = document.getElementById('password').value;
+
+	localStorage['user'] = document.getElementById('user').value;
+	localStorage['tenant'] = document.getElementById('tenant').value;
+	localStorage['com'] = document.getElementById('com').value;
+	localStorage['password'] = document.getElementById('password').value;
+
 	chrome.extension.getBackgroundPage().sendMessage(data);
 }
 document.getElementById('submit').onclick=callBackgroundPage;
+
+if(localStorage['user']) 	document.getElementById('user').value = localStorage['user'];
+if(localStorage['tenant']) 	document.getElementById('tenant').value = localStorage['tenant'];
+if(localStorage['com']) 	document.getElementById('com').value = localStorage['com'];
+if(localStorage['password']) 	document.getElementById('password').value = localStorage['password'];
